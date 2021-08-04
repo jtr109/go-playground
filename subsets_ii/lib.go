6c5ecb935dc33d157bf96ada728b1a55373d97f6
Ryan Li <conbas2019@gmail.com>
2021-08-04 14:49:08 +0800

    feat: wrong answer

--- subsets_ii/lib.go
+++ subsets_ii/lib.go
@@ -1,37 +1,36 @@
 package subsetsii
 
+import (
+	"fmt"
+	"sort"
+)
+
 func subsetsWithDup(nums []int) [][]int {
-	result := [][]int{}
-	for i := 0; i < len(nums); i++ {
-		for j := i; j <= len(nums); j++ {
-			n := nums[i:j]
-			if !contains(result, n) {
-				result = append(result, n)
+	// cache to store subsets, using the length of subsets as key
+	sort.Ints(nums)
+	result, current := [][]int{}, [][]int{}
+	result = append(result, []int{})
+	for i, n := range nums {
+		fmt.Println("n", n)
+		newCurrent := [][]int{}
+		if i != 0 && nums[i-1] == n {
+			// when n is equal to the last one,
+			// the new result of appending to last result is duplicated,
+			// but appending to last current is new
+			for _, item := range current {
+				newCurrent = append(newCurrent, append(item, n))
+			}
+		} else {
+			for _, item := range result {
+				fmt.Println("result in loop", result)
+				fmt.Println("item", item)
+				newCurrent = append(newCurrent, append(item, n))
+				fmt.Println("result after append", result)
+				fmt.Println("---")
 			}
 		}
+		current = newCurrent
+		result = append(result, current...)
 	}
 	return result
-}
-
-func contains(arr [][]int, sub []int) bool {
-	contain := false
-	for _, a := range arr {
-		if equal(a, sub) {
-			contain = true
-			break
-		}
-	}
-	return contain
-}
-
-func equal(current []int, target []int) bool {
-	if len(current) != len(target) {
-		return false
-	}
-	for i, c := range current {
-		if c != target[i] {
-			return false
-		}
-	}
-	return true
 }
--- subsets_ii/lib_test.go
+++ subsets_ii/lib_test.go
@@ -1,7 +1,41 @@
 package subsetsii
 
-import "testing"
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestSubsetWithDup(t *testing.T) {
+	assert.Equal(t, [][]int{{}, {1}, {2}, {1, 2}, {3}, {1, 3}, {2, 3}, {1, 2, 3}}, subsetsWithDup([]int{1, 2, 3}))
+	assert.Equal(t, [][]int{{}, {1}, {2}, {1, 2}, {2, 2}, {1, 2, 2}}, subsetsWithDup([]int{1, 2, 2}))
+}
 
+func TestSubsetWithDupSubmission1(t *testing.T) {
+	expected := [][]int{{}, {1}, {1, 1}, {1, 1, 2}, {1, 1, 2, 2}, {1, 1, 2, 2, 3}, {1, 1, 2, 3}, {1, 1, 3}, {1, 2}, {1, 2, 2}, {1, 2, 2, 3}, {1, 2, 3}, {1, 3}, {2}, {2, 2}, {2, 2, 3}, {2, 3}, {3}}
+	sort.Slice(expected, func(i, j int) bool {
+		if len(expected[i]) != len(expected[j]) {
+			return len(expected[i]) < len(expected[j])
+		}
+		for k := range expected[i] {
+			if expected[i][k] < expected[j][k] {
+				return true
+			}
+		}
+		return false
+	})
+	actual := subsetsWithDup([]int{2, 1, 2, 1, 3})
+	sort.Slice(actual, func(i, j int) bool {
+		if len(actual[i]) != len(actual[j]) {
+			return len(actual[i]) < len(actual[j])
+		}
+		for k := range actual[i] {
+			if actual[i][k] < actual[j][k] {
+				return true
+			}
+		}
+		return false
+	})
+	assert.Equal(t, expected, actual)
 }
