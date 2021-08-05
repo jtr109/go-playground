4fd8448471eed8e901c6316b6af85b533ebeaafd
Ryan Li <conbas2019@gmail.com>
2021-08-05 11:23:40 +0800

    feat: pass

--- path_sum_ii/lib.go
+++ path_sum_ii/lib.go
@@ -1,3 +1,35 @@
 // https://leetcode.com/explore/featured/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3838/
 
 package pathsumii
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+	if root.Val == targetSum && root.Left == nil && root.Right == nil {
+		return [][]int{{root.Val}}
+	}
+	newTargetSum := targetSum - root.Val
+	leftPathSum := [][]int{}
+	res := [][]int{}
+	if root.Left != nil {
+		leftPathSum = pathSum(root.Left, newTargetSum)
+	}
+	rightPathSum := [][]int{}
+	if root.Right != nil {
+		rightPathSum = pathSum(root.Right, newTargetSum)
+	}
+	for _, ps := range leftPathSum {
+		res = append(res, append([]int{root.Val}, ps...))
+	}
+	for _, ps := range rightPathSum {
+		res = append(res, append([]int{root.Val}, ps...))
+	}
+	return res
+}
--- path_sum_ii/lib_test.go
+++ path_sum_ii/lib_test.go
@@ -1,1 +1,53 @@
 package pathsumii
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExample1(t *testing.T) {
+	input := TreeNode{
+		5,
+		&TreeNode{
+			4,
+			&TreeNode{
+				11,
+				&TreeNode{7, nil, nil},
+				&TreeNode{2, nil, nil},
+			},
+			nil,
+		},
+		&TreeNode{
+			8,
+			&TreeNode{13, nil, nil},
+			&TreeNode{
+				4,
+				&TreeNode{5, nil, nil},
+				&TreeNode{1, nil, nil},
+			},
+		},
+	}
+	targetSum := 22
+	expected := [][]int{{5, 4, 11, 2}, {5, 8, 4, 5}}
+	assert.Equal(t, expected, pathSum(&input, targetSum))
+}
+
+func TestSubmission1(t *testing.T) {
+	var input *TreeNode = nil
+	targetSum := 1
+	expected := [][]int{}
+	assert.Equal(t, expected, pathSum(input, targetSum))
+}
+
+func TestSubmission2(t *testing.T) {
+	input := TreeNode{
+		1,
+		nil,
+		&TreeNode{2, nil, nil},
+	}
+	targetSum := 1
+	expected := [][]int{}
+	assert.Equal(t, expected, pathSum(&input, targetSum))
+
+}
