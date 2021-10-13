049ad3a73fe05d91716e98a8244c9b72918a258f
Ryan Li <conbas2019@gmail.com>
2021-10-13 11:29:35 +0800

    feat: add links

--- search_insert_position/lib.go
+++ search_insert_position/lib.go
@@ -1,3 +1,6 @@
+// [Search Insert Position - LeetCode](https://leetcode.com/problems/search-insert-position/submissions/)
+// [代码随想录](https://programmercarl.com/0704.%E4%BA%8C%E5%88%86%E6%9F%A5%E6%89%BE.html#%E7%9B%B8%E5%85%B3%E9%A2%98%E7%9B%AE%E6%8E%A8%E8%8D%90)
+
 package searchinsertposition
 
 func searchInsert(nums []int, target int) int {
