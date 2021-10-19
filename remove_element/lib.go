22234e12f3ffa8696f4d70f0be41c6b8330fd11d
Ryan Li <conbas2019@gmail.com>
2021-10-19 10:06:45 +0800

    feat: resolved

---
+++ remove_element/lib.go
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/remove-element/
+
+package removeelement
+
+func removeElement(nums []int, val int) int {
+	// 如果只是为了得到「不等于 val 的值的总数」其实用一次 O(n) 的简单查询就可以了
+	// 但是为了实现修改 nums，需要做一些额外操作
+	length := len(nums)
+	index := 0
+	for index < length {
+		if nums[index] != val {
+			index++
+			continue
+		}
+		nums[index], nums[length-1] = nums[length-1], nums[index]
+		length--
+	}
+	return length
+}
---
+++ remove_element/lib_test.go
@@ -0,0 +1,21 @@
+package removeelement
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExample1(t *testing.T) {
+	nums := []int{3, 2, 2, 3}
+	val := 3
+	expected := 2
+	assert.Equal(t, expected, removeElement(nums, val))
+}
+
+func TestExample2(t *testing.T) {
+	nums := []int{0, 2, 3, 3, 4, 0, 4, 2}
+	val := 2
+	expected := 5
+	assert.Equal(t, expected, removeElement(nums, val))
+}
