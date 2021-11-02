553c28285958a8d55578a03f242c2b9034341569
Ryan Li <conbas2019@gmail.com>
2021-11-02 10:25:31 +0800

    feat: resolved

---
+++ ransom_note/lib.go
@@ -0,0 +1,20 @@
+package ransomnote
+
+func canConstruct(ransomNote string, magazine string) bool {
+	chars := []int{}
+	for i := 0; i < 26; i++ {
+		chars = append(chars, 0)
+	}
+	for _, r := range magazine {
+		chars[int(r)-int('a')]++
+	}
+	for _, r := range ransomNote {
+		chars[int(r)-int('a')]--
+	}
+	for _, count := range chars {
+		if count < 0 {
+			return false
+		}
+	}
+	return true
+}
