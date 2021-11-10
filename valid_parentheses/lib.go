593d479441a320ca3156b860eea1b67cdd4921d7
Ryan Li <conbas2019@gmail.com>
2021-11-10 11:07:55 +0800

    feat: add link

--- valid_parentheses/lib.go
+++ valid_parentheses/lib.go
@@ -1,20 +1,6 @@
-package validparentheses
+// https://leetcode.com/problems/valid-parentheses/
 
-func ValidParentheses(parens string) bool {
-	mark := 0
-	for _, p := range parens {
-		switch p {
-		case '(':
-			mark++
-		default:
-			mark--
-		}
-		if mark < 0 {
-			return false
-		}
-	}
-	return mark == 0
-}
+package validparentheses
 
 func isValid(s string) bool {
 	stack := []rune{}
