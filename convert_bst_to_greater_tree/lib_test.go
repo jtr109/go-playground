1118be79816df13d58dd5d5be7b0b4b45ede1937
Ryan Li <conbas2019@gmail.com>
2021-12-08 16:48:17 +0800

    feat: add test

--- convert_bst_to_greater_tree/lib_test.go
+++ convert_bst_to_greater_tree/lib_test.go
@@ -26,5 +26,23 @@ func TestExample1(t *testing.T) {
 		nilint.NewNil(),
 		nilint.NewInt(8),
 	})
-	assert.Equal(t, nil, convertBST(root))
+	actual := treenode.ToSlice(convertBST(root))
+	expected := []nilint.NilInt{
+		nilint.NewInt(30),
+		nilint.NewInt(36),
+		nilint.NewInt(21),
+		nilint.NewInt(36),
+		nilint.NewInt(35),
+		nilint.NewInt(26),
+		nilint.NewInt(15),
+		nilint.NewNil(),
+		nilint.NewNil(),
+		nilint.NewNil(),
+		nilint.NewInt(33),
+		nilint.NewNil(),
+		nilint.NewNil(),
+		nilint.NewNil(),
+		nilint.NewInt(8),
+	}
+	assert.Equal(t, expected, actual)
 }
