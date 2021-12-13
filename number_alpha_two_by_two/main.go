91820c4aa36ec0869ca0bc1b279d0e120a283691
Ryan Li <conbas2019@gmail.com>
2021-12-13 21:04:05 +0800

    feat: resolved

---
+++ number_alpha_two_by_two/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	numCh := make(chan struct{})
+	alphaCh := make(chan struct{})
+	ch := make(chan struct{})
+
+	go func() {
+		for a := 'A'; a <= 'Z'; {
+			<-alphaCh
+			fmt.Print(string(a))
+			a++
+			fmt.Print(string(a))
+			a++
+			numCh <- struct{}{}
+		}
+		close(numCh)
+		<-alphaCh // waste one element
+	}()
+
+	go func() {
+		i := 1
+		for range numCh {
+			fmt.Print(i)
+			i++
+			fmt.Print(i)
+			i++
+			alphaCh <- struct{}{}
+		}
+		ch <- struct{}{}
+	}()
+
+	numCh <- struct{}{}
+	<-ch
+}
